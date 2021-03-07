fb7f5a35a3a3881a4a6ccb1e37ac1a473f5a5636
FrontLeejonghun <leejonghun32425@gmail.com>
2021-03-07 13:22:11 +0900

    init

--- src/background.js
+++ src/background.js
@@ -1,3 +1,14 @@
-browser.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  console.log("Hello from the background", request, sender, sendResponse);
+chrome.runtime.onInstalled.addListener(() => {
+  console.log("onInstalled...");
+  // create alarm after extension is installed / upgraded
+  chrome.alarms.create("refresh", { periodInMinutes: 0.1 });
 });
+
+chrome.alarms.onAlarm.addListener(alarm => {
+  console.log(alarm.name); // refresh
+  helloWorld();
+});
+
+function helloWorld() {
+  console.log("Hello, world!");
+}
